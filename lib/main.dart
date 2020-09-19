8045374712ae30a38e7e8e89c0f2a6145a12e18b
Tanishq <tanishq2.0.5@outlook.com>
2020-09-19 14:15:46 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,58 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _dicePageState createState() => _dicePageState();
+}
+
+// ignore: camel_case_types
+class _dicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  void changeDiceFace() {
+    leftDiceNum = Random().nextInt(6) + 1;
+    rightDiceNum = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      changeDiceFace();
+                      print('pressed left');
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNum.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      changeDiceFace();
+                      print('pressed right');
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNum.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
